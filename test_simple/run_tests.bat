abd8c970e99d75ae94437969ead7bca2df21044e
setvisible <sebastien.vavassori@gmail.com>
2017-03-27 18:55:04 +0200

    fixed missing unit test file

--- test_simple/run_tests.bat
+++ test_simple/run_tests.bat
@@ -2,3 +2,5 @@ python .\..\rpt2csv.py "test_001_input.rpt"
 python .\..\rpt2csv.py "test_002_input.rpt"
 python .\..\rpt2csv.py "test_003_input.rpt"
 python .\..\rpt2csv.py "test_004_input.rpt"
+python .\..\rpt2csv.py "test_005_input.rpt"
+PAUSE
